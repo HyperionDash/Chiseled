72ae214d32b15c4bf35f35ac66437b0c5ec22a0f
HyperionDash <maxbatty404@gmail.com>
2025-09-11 13:28:26 +0100

    updated to 1.21.4, loot table datagen is still broken :P

--- src/main/java/net/hyper/chiseled/datagen/ModModelProvider.java
+++ src/main/java/net/hyper/chiseled/datagen/ModModelProvider.java
@@ -1,11 +1,10 @@
 package net.hyper.chiseled.datagen;
 
+import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
 import net.hyper.chiseled.block.ModBlocks;
-import net.minecraft.block.Block;
-import net.minecraft.data.client.BlockStateModelGenerator;
-import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.client.data.BlockStateModelGenerator;
+import net.minecraft.client.data.ItemModelGenerator;
 
 public class ModModelProvider extends FabricModelProvider {
     public ModModelProvider(FabricDataOutput output) {

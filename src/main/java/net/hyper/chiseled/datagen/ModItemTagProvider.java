9aba7fee28cb58136284bfdbcae997960dc394bb
HyperionDash <maxbatty404@gmail.com>
2025-09-22 13:06:50 +0100

    will this work?

---
+++ src/main/java/net/hyper/chiseled/datagen/ModItemTagProvider.java
@@ -0,0 +1,22 @@
+package net.hyper.chiseled.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.hyper.chiseled.block.ModBlocks;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.tag.ItemTags;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+        valueLookupBuilder(ItemTags.WALLS)
+                .add(Item.fromBlock(ModBlocks.TEST_BLOCK_WALL));
+    }
+}
--- src/main/java/net/hyper/chiseled/datagen/ModRecipeProvider.java
+++
@@ -1,4 +0,0 @@
-package net.hyper.chiseled.datagen;
-
-public class ModRecipeProvider {
-}
